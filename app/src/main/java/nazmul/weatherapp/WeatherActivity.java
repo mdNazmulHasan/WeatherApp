e5f66adf0aac3d3b501fb18a9eb71ffc0cad1cb0
mdNazmulHasan <najmul.hasan10@gmail.com>
2018-05-26 20:03:04 +0600

    displayed weather data in a list

--- app/src/main/java/nazmul/weatherapp/WeatherActivity.java
+++ app/src/main/java/nazmul/weatherapp/WeatherActivity.java
@@ -1,9 +1,13 @@
 package nazmul.weatherapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import nazmul.weatherapp.adapters.WeatherAdapter;
 import nazmul.weatherapp.models.WeatherData;
 import nazmul.weatherapp.utils.RetrofitClient;
 import nazmul.weatherapp.webapi.WeatherApi;
@@ -13,30 +17,55 @@ import retrofit2.Response;
 
 public class WeatherActivity extends AppCompatActivity {
     WeatherApi weatherApi;
+    ListView weatherdataListView;
+    ProgressDialog progDailog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        weatherdataListView=findViewById(R.id.weatherdataListView);
+        progDailog=new ProgressDialog(this);
         getWeatherData();
+
     }
 
     private void getWeatherData() {
+        showprogessdialog();
         weatherApi= RetrofitClient.getRetrofitClient().create(WeatherApi.class);
         Call<WeatherData> weatherDataCall=weatherApi.getAllWeatherInfo();
         weatherDataCall.enqueue(new Callback<WeatherData>() {
             @Override
             public void onResponse(Call<WeatherData> call, Response<WeatherData> response) {
+                hiddenProgressDialog();
                 WeatherData weatherData=response.body();
-                Log.e("weather data ", "onResponse: "+weatherData.getMessage() );
+                WeatherAdapter weatherAdapter=new WeatherAdapter(WeatherActivity.this,weatherData.getList());
+                weatherdataListView.setAdapter(weatherAdapter);
             }
 
             @Override
             public void onFailure(Call<WeatherData> call, Throwable t) {
+                hiddenProgressDialog();
+                Toast.makeText(WeatherActivity.this, ""+t.getMessage(), Toast.LENGTH_SHORT).show();
 
             }
         });
 
 
+    }
+    public void showprogessdialog() {
+        if (progDailog != null) {
+            progDailog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            progDailog.setMessage("Please wait...");
+            progDailog.setCancelable(false);
+            progDailog.show();
+
+        }
+    }
+
+    public void hiddenProgressDialog() {
+        if (progDailog != null) {
+            progDailog.dismiss();
+        }
     }
 }
---
+++ app/src/main/java/nazmul/weatherapp/adapters/WeatherAdapter.java
@@ -0,0 +1,40 @@
+package nazmul.weatherapp.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import nazmul.weatherapp.R;
+import nazmul.weatherapp.models.List;
+
+public class WeatherAdapter extends ArrayAdapter<List> {
+    private Context context;
+    private java.util.List<List>weatherDataList;
+
+    public WeatherAdapter(@NonNull Context context,@NonNull java.util.List<List> objects) {
+        super(context, R.layout.weather_row, objects);
+        this.context=context;
+        this.weatherDataList=objects;
+    }
+
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view= LayoutInflater.from(context).inflate(R.layout.weather_row,parent,false);
+        TextView cityText=view.findViewById(R.id.city_text);
+        TextView conditionText=view.findViewById(R.id.condition_text);
+        TextView temperatureText=view.findViewById(R.id.temperature_text);
+        List list=weatherDataList.get(position);
+        cityText.setText(list.getName());
+        conditionText.setText(list.getWeather().get(0).getMain());
+        char degree = '\u00B0';
+        double temp=list.getMain().getTemp()-273.15;
+        temperatureText.setText(String.format("%.1f",temp)+""+degree+"C");
+        return view;
+    }
+}
