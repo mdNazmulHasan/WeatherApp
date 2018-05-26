2f7af999a5a21818d994a42e1d328eb65d505d99
mdNazmulHasan <najmul.hasan10@gmail.com>
2018-05-26 17:24:13 +0600

    basic config

---
+++ app/src/main/java/nazmul/weatherapp/models/WeatherData.java
@@ -0,0 +1,54 @@
+
+package nazmul.weatherapp.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class WeatherData {
+
+    @SerializedName("message")
+    @Expose
+    private String message;
+    @SerializedName("cod")
+    @Expose
+    private String cod;
+    @SerializedName("count")
+    @Expose
+    private Integer count;
+    @SerializedName("list")
+    @Expose
+    private java.util.List<com.example.List> list = null;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getCod() {
+        return cod;
+    }
+
+    public void setCod(String cod) {
+        this.cod = cod;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public java.util.List<com.example.List> getList() {
+        return list;
+    }
+
+    public void setList(java.util.List<com.example.List> list) {
+        this.list = list;
+    }
+
+}
